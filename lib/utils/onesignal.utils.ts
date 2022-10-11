b356729f2759b6b5903acc814611deeb25713b5b
mughieams <mughieams24@gmail.com>
2022-10-11 16:40:15 +0800

    build: change onesignal dependency

--- lib/utils/onesignal.utils.ts
+++ lib/utils/onesignal.utils.ts
@@ -1,29 +1,10 @@
 import { OneSignalClient } from './onesignal.interface';
 import { OPTIONS_TYPE } from './onesignal.module-definition';
 
-import * as OneSignal from '@onesignal/node-onesignal';
+import * as OneSignal from 'onesignal-node';
 
 export function createOneSignalClient(
   options: typeof OPTIONS_TYPE,
 ): OneSignalClient {
-  const app_key_provider = {
-    getToken() {
-      return options.appKey;
-    },
-  };
-
-  const user_key_provider = {
-    getToken() {
-      return options.userKey;
-    },
-  };
-
-  const configuration = OneSignal.createConfiguration({
-    authMethods: {
-      app_key: { tokenProvider: app_key_provider },
-      user_key: { tokenProvider: user_key_provider },
-    },
-  });
-
-  return new OneSignal.DefaultApi(configuration);
+  return new OneSignal.Client(options.appId, options.apiKey);
 }
